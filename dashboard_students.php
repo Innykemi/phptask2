426bf8e0e616d85e8b28e1f6bbf5aaa6c55439df
Iniobong Oluwakemi <oluwakemiraimi.nig@gmail.com>
2020-04-23 19:55:30 +0100

    Dashboard completed

--- dashboard_students.php
+++ dashboard_students.php
@@ -1,7 +1,6 @@
 <?php
 include ("lib/header.php");
-    if(!isset($_SESSION['loggedin'])) {
-        //redirect to dashboard
+    if (isset($_SESSION['loggedin']) && $_SESSION['designation'] != 'Student' || empty($_SESSION['loggedin'])) {
         header("location: login.php");
     }
 include ("lib/menu.php");
@@ -10,15 +9,49 @@ include ("lib/menu.php");
     <main role="main">
         <div class="container">
             <div class="row">
-                <div class="col-md-12 mt-5">
-                    <h1>Dashboard</h1>
-                    <h3>Welcome <?php echo $_SESSION['full_name'];?><small> (<?php echo $_SESSION['department'];?>)</small></h3>
-                    <p>You're logged in as a <strong><?php echo $_SESSION['designation'];?></strong>
-                    </p>
-                    <p>
-                        Registered: <?php echo $_SESSION['registration_date'];?><br/>
-                        Last Login: <?php echo $_SESSION['last_login'];?>
-                    </p>
+                <div class="col-md-12">
+                    <div class="col-md-4 float-right mt-4">
+                        <?php
+                            if (isset($_SESSION['message']) && !empty($_SESSION['message'])){
+                                echo 
+                                    "<div class='alert alert-success alert-dismissible fade show' role='alert'>" 
+                                        . $_SESSION["message"] .
+                                        "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>",
+                                    "</div>";
+                                //session_destroy();
+                            }
+                        ?>
+                    </div>
+                    
+                </div>
+                <div class="col-md-12 mt-2 row">
+                    <div class="col-md-5 col-12">
+                        <h3 class="purple">Welcome <?php echo $_SESSION['full_name'];?></h3>
+                        <p>You're logged in as a <strong><?php echo $_SESSION['designation'];?></strong>
+                        </p>
+                        <a href="paybill.php" style="margin-right: 1em;"><button>Pay Bill</button></a>
+                        <a href="book_appointment.php"><button>Book Appointment</button></a>
+                    </div>
+                    <div class="col-md-7 col-12 row">
+                        <div class="col-md-4">
+                            <div class="py-3 px-3 mb-2 bg-white rounded shadow-sm">
+                                <h5>Department</h5>
+                                <p><?php echo $_SESSION['department'];?></p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="py-3 px-3 mb-2 bg-white rounded shadow-sm">
+                                <h5>Registered</h5>
+                                <p><?php echo $_SESSION['registration_date'];?></p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="py-3 px-3 mb-2 bg-white rounded shadow-sm">
+                                <h5>Last Login</h5>
+                                <p><?php echo $_SESSION['last_login'];?></p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
