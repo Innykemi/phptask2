fcf3a704bb4881b62be8929082cd19d2f53206d4
Iniobong Oluwakemi <oluwakemiraimi.nig@gmail.com>
2020-04-24 23:49:13 +0100

    modified page

--- forgot.php
+++ forgot.php
@@ -8,18 +8,9 @@
 ?>
     <main role="main" class="container">
         <div class="my-5 p-3 px-4 max-width-35 mx-auto bg-white rounded shadow-sm">
-            <?php
-                if (isset($_SESSION['message']) && !empty($_SESSION['message'])){
-                    echo 
-                        "<div class='alert alert-success' role='alert'>" 
-                            . $_SESSION["message"] .
-                        "</div>";
-                    session_destroy();
-                }
-            ?>
             <h2>Forgot Password</h2>
             <p>Please enter email associated with this account</p>
-            <form action="process/processforgot.php" method="post">
+            <form action="process/processforgot2.php" method="post">
                 <?php
                     if (isset($_SESSION['error']) && !empty($_SESSION['error'])){
                         echo 
@@ -27,7 +18,7 @@
                                 . $_SESSION['error'] . "<br/>"
                                 . $_SESSION["emailErr"] .
                             "</div>";
-                        session_destroy();
+                        unset($_SESSION['error']);
                     }
                 ?>
                 <div class="form-item">
